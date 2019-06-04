2559d62b3f37a9650f1b71ec9b0094e440b92400
ryugo harada <r.harada1811@morijyobi.ac.jp>
2019-06-04 11:17:10 +0900

    [modify]Conflict.java

--- Conflict.java
+++ Conflict.java
@@ -1,6 +1,9 @@
 
 public class Conflict{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		for(int i = 0;i<9;i++){
+			int rnd = (int)(Math.random() * 100);
+			System.out.println(rnd);
+		}
 	}
 }
